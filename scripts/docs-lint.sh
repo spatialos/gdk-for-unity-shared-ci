a8b8f529af5e277c5b28e0195a3fd8a8a82bda7b
Jamie Brynes <jamiebrynes@improbable.io>
2018-10-11 16:21:38 +0100

    remove not-shared-bits

--- scripts/docs-lint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e -u -x -o pipefail
-
-source ci/includes/pinned-tools.sh
-source ci/includes/profiling.sh
-
-markStartOfBlock "Running Docs Linter"
-dotnet run -p tools/DocsLinter/DocsLinter.csproj 
-markEndOfBlock "Running Docs Linter"
